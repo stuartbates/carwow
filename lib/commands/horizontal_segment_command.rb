7a1235c2fed63929a521e3406e81b644914ea7c5
Stuart Bates <hello@stuartbates.com>
2017-11-01 12:15:43 +0000

    Added command to draw horizontal segment onto bitmap

---
+++ lib/commands/horizontal_segment_command.rb
@@ -0,0 +1,30 @@
+require 'commands/base_command'
+require 'commands/colour_pixel_command'
+
+class HorizontalSegmentCommand < BaseCommand
+
+  def execute(bitmap)
+    (x1..x2).inject(bitmap) do |bitmap, x|
+      ColourPixelCommand.new([x, y, colour]).execute(bitmap)
+    end
+  end
+
+  private
+
+  def x1
+    args[1]
+  end
+
+  def x2
+    args[2]
+  end
+
+  def y
+    args[0]
+  end
+
+  def colour
+    args[3]
+  end
+
+end
