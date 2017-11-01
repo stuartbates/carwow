530142972cc8d00150dd8ebee8d31cbe853fd5e3
Stuart Bates <hello@stuartbates.com>
2017-11-01 13:22:14 +0000

    Added error handling for if file does not exist

--- lib/command_file_parser.rb
+++ lib/command_file_parser.rb
@@ -1,14 +1,23 @@
 require 'command_mapper'
+require 'custom_errors'
 
 class CommandFileParser
 
-  attr_reader :file_name
+  attr_reader :file_name, :commands
 
   def initialize(file_name)
     @file_name = file_name
+    ensure_file_exists
+    @commands = parse_commands
   end
 
-  def commands
+  private
+
+  def ensure_file_exists
+    raise CommandFileNotFound unless File.exists?(file_name)
+  end
+
+  def parse_commands
     lines = IO.readlines(file_name)
     lines.map do |line|
       parts = line.strip.split(/\s+/)
--- spec/command_file_parser_spec.rb
+++ spec/command_file_parser_spec.rb
@@ -1,5 +1,6 @@
 require 'command_file_parser'
 require 'commands/base_command'
+require 'custom_errors'
 
 describe CommandFileParser do
 
@@ -11,6 +12,16 @@ describe CommandFileParser do
 
     it 'returns an array of commands' do
       expect(parser.commands).to all(be_kind_of(BaseCommand))
+    end
+
+  end
+
+  context 'file does not exist' do
+
+    let(:file_name) { 'spec/fixtures/inputs/non_existent_file.txt' }
+
+    it 'raises a CommandFileNotFound error' do
+      expect { parser }.to raise_error(CommandFileNotFound)
     end
 
   end
