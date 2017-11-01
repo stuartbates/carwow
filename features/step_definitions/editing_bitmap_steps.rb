abf03ac0079587716ad9b28d7ccdf1e87a95cdd5
Stuart Bates <hello@stuartbates.com>
2017-11-01 13:16:02 +0000

    Attempt to fix cucumber stdout check

--- features/step_definitions/editing_bitmap_steps.rb
+++ features/step_definitions/editing_bitmap_steps.rb
@@ -1,8 +1,12 @@
 Then(/^(?:the )?(output|stderr|stdout) should match the template at: "([^"]*)"$/) do |channel, template|
   expected = File.open(template, 'r') { |f| f.read }.chomp
-  combined_output = all_commands.map do |c|
-    c.stop(announcer)
-    c.send(channel.to_sym).chomp
-  end.join("\n")
-  expect(combined_output).to eq(expected)
+  if Aruba::VERSION < '1.0'
+    combined_output = all_commands.map do |c|
+      c.stop(aruba.announcer)
+      c.send(channel.to_sym).chomp
+    end.join("\n")
+    expect(combined_output).to eq(expected)
+  else
+    expect(all_commands).to eq(expected)
+  end
 end
